e4cc7ca912f46a5bd73b19f65474931cfc90f640
Joern Bernhardt <jb@campudus.com>
2017-08-09 23:34:36 +0200

    Add a test to test testing
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 02-purescript-with-pulp/test/Main.purs
+++ 02-purescript-with-pulp/test/Main.purs
@@ -1,9 +1,11 @@
 module Test.Main where
 
-import Prelude
+import Prelude (Unit, (==))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Test.Assert (ASSERT, assert)
+import Main (greeting)
+
+main :: forall e. Eff (assert :: ASSERT | e) Unit
 main = do
-  log "You should add some tests."
+  assert ((greeting "world") == "Hello world!")
