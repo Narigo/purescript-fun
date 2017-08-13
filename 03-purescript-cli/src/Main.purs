078e63c3021113462c73976f32f08ced909d9bdd
Joern Bernhardt <jb@campudus.com>
2017-08-13 19:25:48 +0200

    Make main as in 01
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 03-purescript-cli/src/Main.purs
+++ 03-purescript-cli/src/Main.purs
@@ -1,12 +1,10 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Array
 
-foreign import process :: forall props. { | props }
+greeting :: Array String -> String
+greeting arr = "Hello " <> (show (head arr)) <> "!"
 
-main :: Array String -> forall e. Eff (console :: CONSOLE | e) Unit
-main input = do
-  log "Hello " <> (show (head input)) <> "!"
+main :: Array String -> String
+main arr = greeting arr
