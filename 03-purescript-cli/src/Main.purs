b07664401f1a700d910fd87c4d77d5efb6f9bfeb
Joern Bernhardt <jb@campudus.com>
2017-08-16 01:48:03 +0200

    Use bind operator (>>=) to get head of tail of tail
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 03-purescript-cli/src/Main.purs
+++ 03-purescript-cli/src/Main.purs
@@ -3,13 +3,17 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Array (head)
+import Data.Array (head, tail)
+import Data.Maybe
 import Node.Process (PROCESS, argv)
 
 greeting :: String -> String
 greeting who = "Hello " <> who <> "!"
 
+getArgs :: Array String -> Maybe (String)
+getArgs args = tail args >>= tail >>= head
+
 main :: forall eff. Eff (console :: CONSOLE, process :: PROCESS | eff) Unit
 main = do
   arr <- argv
-  log (greeting (show (head arr)))
+  log (greeting (show (getArgs arr)))
