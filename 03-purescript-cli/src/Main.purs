0534fd437164374f29be535cc01c43e6afa2df67
Joern Bernhardt <jb@campudus.com>
2017-08-18 19:32:12 +0200

    Use specified imports and rename getArgs to getArgsBind
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 03-purescript-cli/src/Main.purs
+++ 03-purescript-cli/src/Main.purs
@@ -1,17 +1,17 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, (<>), (>>=))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Array (head, tail)
-import Data.Maybe
+import Data.Maybe (Maybe(..))
 import Node.Process (PROCESS, argv)
 
 greeting :: String -> String
 greeting who = "Hello " <> who <> "!"
 
-getArgs :: Array String -> Maybe (String)
-getArgs args = tail args >>= tail >>= head
+getArgsBind :: Array String -> Maybe (String)
+getArgsBind args = tail args >>= tail >>= head
 
 getArgsDo :: Array String -> Maybe (String)
 getArgsDo args = do
@@ -26,4 +26,4 @@ get Nothing    = ""
 main :: forall eff. Eff (console :: CONSOLE, process :: PROCESS | eff) Unit
 main = do
   arr <- argv
-  log (greeting (get (getArgsDo arr)))
+  log (greeting (get (getArgsBind arr)))
