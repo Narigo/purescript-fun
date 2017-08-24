24f3f60b0ce22119d644eb1c261a5c058627fb91
Joern Bernhardt <jb@campudus.com>
2017-08-24 09:47:42 +0200

    Add a first test with purescript-test-unit
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 04-purescript-testing/src/Main.purs
+++ 04-purescript-testing/src/Main.purs
@@ -3,6 +3,10 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Array
+
+sum :: Array Int -> Int
+sum _ = 0
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
--- 04-purescript-testing/test/Main.purs
+++ 04-purescript-testing/test/Main.purs
@@ -1,9 +1,22 @@
 module Test.Main where
 
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude (Unit, ($), (==))
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Aff.AVar
+
+import Test.Unit (suite, test)
+import Test.Unit.Console (TESTOUTPUT)
+
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
+import Main (sum)
+
+main :: forall e. Eff (console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR | e) Unit
+main = runTest do
+  suite "sum" do
+    test "should be zero for empty list" do
+      Assert.assert "empty array should be 0" $ (sum []) == 0
+
