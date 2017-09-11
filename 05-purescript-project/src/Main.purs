23dd1f59c92259d230c06bbefabeb20236575714
Joern Bernhardt <jb@campudus.com>
2017-09-11 20:15:37 +0200

    Add a few types
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 05-purescript-project/src/Main.purs
+++ 05-purescript-project/src/Main.purs
@@ -1,9 +1,45 @@
 module Main where
 
 import Prelude
+import Data.List
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
+
+showTable :: Table -> String
+showTable table = showColumns table.columns <> "\n" <> showRows table.rows
+
+showColumns :: List Column -> String
+showColumns columns = "" <> show (map showColumn columns)
+
+showRows :: List Row -> String
+showRows row = "" <> show (map showRow row)
+
+showColumn :: Column -> String
+showColumn column = "" <> show column.id <> ": " <> column.name
+
+showRow :: Row -> String
+showRow row = "" <> show row.id <> ": " <> show (map showCell row.cells)
+
+showCell :: String -> String
+showCell cell = "Cell(" <> show cell <> ")"
+
+type Column = {
+    id :: Int,
+    name :: String
+  }
+
+type Cell = String
+
+type Row = {
+    id :: Int,
+    cells :: List Cell
+  }
+
+type Table = {
+    columns :: List Column,
+    rows :: List Row
+  }
