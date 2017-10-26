d5707d258af29f679f4d32ede400cc3bcfd0828c
Joern Bernhardt <jb@campudus.com>
2017-10-26 23:32:35 +0200

    Define some types for third try
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- 05-purescript-project/src/ThirdTab.purs
+++ 05-purescript-project/src/ThirdTab.purs
@@ -6,14 +6,14 @@ import Data.List
 
 class ColT a
 
-type Cells a = List a
+type Cells a = ColT a => List a
+
+newtype Col a = Col { id :: Int, name :: String }
 
 newtype ThirdTab colTypes = ThirdTab
   { cols :: List colTypes
   , rows :: List (Cells colTypes)
   }
-
-
 
 empty :: forall a. ColT a => ThirdTab a
 empty = ThirdTab
@@ -21,5 +21,5 @@ empty = ThirdTab
   , rows : Nil
   }
 
-addColumn :: forall a. ColT a => Int -> String -> ThirdTab a -> ThirdTab a
-addColumn id name (ThirdTab table) = ThirdTab { cols: table.cols, rows: table.rows }
+addColumn :: forall a. ColT a => Int -> String -> ThirdTab a -> ThirdTab _
+addColumn id name (ThirdTab table) = ThirdTab { cols: table.cols, rows: Nil }
